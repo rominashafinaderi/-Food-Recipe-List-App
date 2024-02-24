3a635e2ed3be84fd9959ec5b8230edcdd11b9384
rominaShafiNaderi <romina.shafinaderi@gmail.com>
2024-02-24 14:02:57 +0330

    AGAIN

--- lib/models/recipe.api.dart
+++ lib/models/recipe.api.dart
@@ -17,36 +17,57 @@ import 'package:visualstudio_test/models/recipe.dart';
 
 // req.end(function (res) {
 // 	if (res.error) throw new Error(res.error);
-
+ 
 // 	console.log(res.body);
 // });
 class RecipeApi {
-  static Future<List<Recipe>> getRecipe() async {
-    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
-        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
 
-    final response = await http.get(uri, headers: {
-      "x-rapidapi-key": "cb24f2f391msh9f991ec16d0f3c1p19dbe8jsn0e52ed227be8",
-      "x-rapidapi-host": "yummly2.p.rapidapi.com",
-      "useQueryString": "true"
+  static Future<List<Recipe>> getRecipe() async{
+    var  uri = Uri.http('yummly2.p.rapidapi.com','/feeds/list',{
+	'limit': '24',
+	'start': '0'
+
     });
+    final response =  await http.get(uri,headers: {
+    	'X-RapidAPI-Key': 'cb24f2f391msh9f991ec16d0f3c1p19dbe8jsn0e52ed227be8',
+	    'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+    });
+     Map data = jsonDecode(response.body); 
+     List _temp  = [];
+       for ( var i in data['feed']){
+        _temp.add(i['content']['details']);
+       }
+       return Recipe.recipesFromSnapshot(_temp);
 
-    if (response.statusCode == 200) {
-      Map data = jsonDecode(response.body);
-      List _temp = [];
-
-      if (data['feed'] != null) {
-        for (var i in data['feed']) {
-          _temp.add(i['content']['details']);
-        }
-      } else {
-        print('Feed data is null or not iterable');
-      }
-
-      return Recipe.recipesFromSnapshot(_temp);
-    } else {
-      print('Request failed with status: ${response.statusCode}');
-      return []; // Return an empty list or handle the error accordingly
-    }
   }
+
+   
+  // static Future<List<Recipe>> getRecipe() async {
+  //   var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+  //       {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+  //   final response = await http.get(uri, headers: {
+  //     "x-rapidapi-key": "cb24f2f391msh9f991ec16d0f3c1p19dbe8jsn0e52ed227be8",
+  //     "x-rapidapi-host": "yummly2.p.rapidapi.com",
+  //     "useQueryString": "true"
+  //   });
+
+  //   if (response.statusCode == 200) {
+  //     Map data = jsonDecode(response.body);
+  //     List _temp = [];
+
+  //     if (data['feed'] != null) {
+  //       for (var i in data['feed']) {
+  //         _temp.add(i['content']['details']);
+  //       }
+  //     } else {
+  //       print('Feed data is null or not iterable');
+  //     }
+
+  //     return Recipe.recipesFromSnapshot(_temp);
+  //   } else {
+  //     print('Request failed with status: ${response.statusCode}');
+  //     return []; // Return an empty list or handle the error accordingly
+  //   }
+  // }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -10,7 +10,7 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState    extends State<HomePage> {
  late  List<Recipe> _recipe;
   bool isLoading = true;
   @override
@@ -19,13 +19,19 @@ class _HomePageState extends State<HomePage> {
     getRecipes();
   }
 
-  Future<void> getRecipes() async{
+  Future <void> getRecipes() async{
     _recipe = await RecipeApi.getRecipe();
     setState(() {
-      isLoading = false;
+      isLoading = true;
     });
-    print(_recipe);
   }
+  // Future<void> getRecipes() async{
+  //   _recipe = await RecipeApi.getRecipe();
+  //   setState(() {
+  //     isLoading = false;
+  //   });
+  //   print(_recipe);
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +45,7 @@ class _HomePageState extends State<HomePage> {
         ]),
       ),
       body:isLoading?
-       Center(child: CircularProgressIndicator(),):
+        const Center(child: CircularProgressIndicator(),):
         ListView.builder(
           itemCount: _recipe.length,
           itemBuilder: (context,index){
